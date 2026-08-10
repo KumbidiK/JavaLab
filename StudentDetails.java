2cf3af578bfdbc42d51cb9e5e8311ed93d993123
Achyuth_Prathap <154974213+KumbidiK@users.noreply.github.com>
2026-08-10 10:25:40 +0530

    Create StudentDetails.java

---
+++ StudentDetails.java
@@ -0,0 +1,19 @@
+class Student {
+    String name;
+    int rollNo;
+    double mark;
+}
+
+class StudentDetails {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.name = "John";
+        s.rollNo = 101;
+        s.mark = 85.5;
+
+        System.out.println("Name: " + s.name);
+        System.out.println("Roll No: " + s.rollNo);
+        System.out.println("Mark: " + s.mark);
+    }
+}
