2e0489d21f5a8c0649666f0c8996f00b62d42ee5
Achyuth_Prathap <154974213+KumbidiK@users.noreply.github.com>
2026-08-10 11:15:55 +0530

    Create BookDetails.java

---
+++ BookDetails.java
@@ -0,0 +1,34 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class BookDetails {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        Book book2 = new Book("Randamoozham", "M T Vasudevan Nair", 399.0);
+
+        book1.display();
+        System.out.println();
+        book2.display();
+    }
+}
