a5c437d732c1aedce3040ab624e9264a9f8d0947
Achyuth_Prathap <154974213+KumbidiK@users.noreply.github.com>
2026-08-14 14:56:45 +0530

    Create StudentManage.java

---
+++ StudentManage.java
@@ -0,0 +1,69 @@
+class Student {
+    String name;
+    int rollNo;
+    double mark;
+
+    Student() {
+        name = "Unknown";
+        rollNo = 0;
+        mark = 0;
+    }
+
+    Student(String name, int rollNo, double mark) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.mark = mark;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Mark: " + mark);
+    }
+
+    void calculateGrade(int mark) {
+        if (mark >= 90)
+            System.out.println("Grade: A");
+        else if (mark >= 75)
+            System.out.println("Grade: B");
+        else if (mark >= 50)
+            System.out.println("Grade: C");
+        else
+            System.out.println("Grade: F");
+    }
+
+    void calculateGrade(double mark) {
+        if (mark >= 90)
+            System.out.println("Grade: A");
+        else if (mark >= 75)
+            System.out.println("Grade: B");
+        else if (mark >= 50)
+            System.out.println("Grade: C");
+        else
+            System.out.println("Grade: F");
+    }
+}
+
+class StudentManage {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Anu", 101, 85.5);
+        Student s3 = new Student("Rahul", 102, 92);
+
+        s1.display();
+        s1.calculateGrade(45);
+
+        s2.display();
+        s2.calculateGrade(85.5);
+
+        s3.display();
+        s3.calculateGrade(92);
+
+        s1 = null;
+        s2 = null;
+
+        System.gc();
+
+        System.out.println("Garbage collection requested");
+    }
+}
