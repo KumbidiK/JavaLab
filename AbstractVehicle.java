9c7433b117eb2f7f376529cf0ef0c8d20af37cd6
Achyuth_Prathap <154974213+KumbidiK@users.noreply.github.com>
2026-09-14 11:07:39 +0530

    Create AbstractVehicle.java

---
+++ AbstractVehicle.java
@@ -0,0 +1,34 @@
+abstract class Vehicle {
+    abstract void start();
+
+    void display() {
+        System.out.println("This is a vehicle");
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void start() {
+        System.out.println("Car starts with a key");
+    }
+}
+
+class Bike extends Vehicle {
+    @Override
+    void start() {
+        System.out.println("Bike starts with a self-start button");
+    }
+}
+
+public class AbstractVehicle {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Bike bike = new Bike();
+
+        car.display();
+        car.start();
+
+        bike.display();
+        bike.start();
+    }
+}
